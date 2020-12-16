eea2088fb02fce4a5084dd124d1ceff0656fb8a6
CrazyPie2025 <mdmirasibalhasan@gmail.com>
2020-12-16 12:13:02 +0600

    Message

---
+++ Grocery_Store/resources/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	// jQuery code
+
+	//////////////////////// Prevent closing from click inside dropdown
+    $(document).on('click', '.dropdown-menu', function (e) {
+      e.stopPropagation();
+    });
+    
+	
+});
+$('.your-slider').carouselLineArrow({
+
+  // duration in ms
+  lineDur: 5000,
+
+  // or 'top'
+  linePosition: 'bottom',
+
+  // height
+  lineHeight: '5px',
+
+  // background color
+  lineColor: '#000'
+  
+});
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
